bd37944c2beca5e76234d976ff208abf41a2fa70
Francisco Gallegos <fgallegosgo@gmail.com>
2021-10-14 21:26:18 -0600

    @ViewChild - get references to HTML objects

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  @ViewChild('txtSearch') txtSearch!:ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  search( ) {
+    
+    const value = this.txtSearch.nativeElement.value;
+
+    console.log(value);
+
+    this.txtSearch.nativeElement.value = "";
   }
-
 }
