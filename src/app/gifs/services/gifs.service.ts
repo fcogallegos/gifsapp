214da08e809f32affd8d7a962c613c938e1f0bb4
Francisco Gallegos <fgallegosgo@gmail.com>
2021-10-19 20:21:46 -0600

    how to make a http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,5 @@
+
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,11 +7,14 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'GXvcJfEFXeFGBm4Uvj8by9bMwDpDGb8F';
   private _history: string[] = [];
 
   get history() {
     return [...this._history];
   }
+
+  constructor(private http: HttpClient) {}
 
   searchGifs( query: string ) {
     
@@ -17,9 +22,13 @@ export class GifsService {
 
     if( !this._history.includes(query) ) {
       this._history.unshift(query);
+      this._history = this._history.splice(0,10);
     }
 
-    this._history = this._history.splice(0,10);
-    console.log( this._history );
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=GXvcJfEFXeFGBm4Uvj8by9bMwDpDGb8F&q=cars')
+      .subscribe( (resp: any) => {
+        console.log(resp.data);
+      });
+    
   }
 }
